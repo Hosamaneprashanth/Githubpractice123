870e21d4c9d9a44c3d22584620219cd8e671fa35
Hosamaneprashanth <prashanth.18035@gmail.com>
2023-09-29 21:07:24 +0530

    secoond commigt

--- Githubpractice/src/test/java/practice/Githubpractice.java
+++ Githubpractice/src/test/java/practice/Githubpractice.java
@@ -4,6 +4,7 @@ public class Githubpractice {
 	public static void main(String[] args) {
 		System.out.println("hi kiran");
 		System.out.println("hi prashanth");
+		System.out.println("jsi");
 		
 		
 	}
